ffaaec42438fdd677a960cf1025340a8b772283c
heisenberg890 <calamatta89@gmail.com>
2014-12-09 00:29:35 -0500

    Added zombies, added a zombie boss, added animations for the zombie boss and programmed the enemies random movement. Also changed hero to a bus.

--- src/game.js
+++ src/game.js
@@ -1,74 +1,16 @@
-EnemyTank = function (index, game, player, bullets) {
-
-    var x = game.world.randomX;
-    var y = game.world.randomY;
-
-    this.game = game;
-    this.health = 3;
-    this.player = player;
-    this.bullets = bullets;
-    this.fireRate = 1000;
-    this.nextFire = 0;
-    this.alive = true;
-
-    this.shadow = game.add.sprite(x, y, 'enemy', 'shadow');
-    this.tank = game.add.sprite(x, y, 'enemy', 'tank1');
-    this.turret = game.add.sprite(x, y, 'enemy', 'turret');
-
-    this.shadow.anchor.set(0.5);
-    this.tank.anchor.set(0.5);
-    this.turret.anchor.set(0.3, 0.5);
-
-    this.tank.name = index.toString();
-    game.physics.enable(this.tank, Phaser.Physics.ARCADE);
-    this.tank.body.immovable = false;
-    this.tank.body.collideWorldBounds = true;
-    this.tank.body.bounce.setTo(1, 1);
-
-    this.tank.angle = game.rnd.angle();
-
-    game.physics.arcade.velocityFromRotation(this.tank.rotation, 100, this.tank.body.velocity);
-
-};
-
-
-EnemyTank.prototype.damage = function() {
-
-    this.health -= 1;
-
-    if (this.health <= 0)
-    {
-        this.alive = false;
-
-        this.shadow.kill();
-        this.tank.kill();
-        this.turret.kill();
-
-        return true;
-    }
-
-    return false;
-
-}
-
-
 var game = new Phaser.Game(1000, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
 //load the images
-    game.load.image('hero', 'assets/sprites/car.png');
+    game.load.image('hero', 'assets/sprites/bus.png');
     game.load.image('road', 'assets/backgrounds/A-road.jpg');
-    //game.load.image('cars', 'assets/sprites/car.png');
     game.load.spritesheet('innocent', 'assets/sprites/innocent.png', 50, 69);
-    
-    
-    game.load.image('bullet', 'assets/games/tanks/bullet.png');
-    game.load.image('kaboom', 'assets/games/tanks/innocent.png');
-    game.load.image('enemy', 'assets/sprites/car.png');
-
+    game.load.image('minion', 'assets/sprites/minion.png');
+    game.load.spritesheet('boss', 'assets/sprites/Zombie.png', 158, 255);
 }
 
+var boss;
 var land;
 var hero;
 var currentSpeed = 0;
@@ -76,7 +18,8 @@ var cursors;
 var group;
 var innocent;
 var innocentsLeft = 0;
-var text;
+var zombiesLeft = 0;
+var minions;
 
 
 
@@ -94,6 +37,27 @@ function create() {
     land.fixedToCamera = true;
     
     
+   
+   
+    
+ /*   for (var counter = 0; counter < 20; counter++)
+    {
+        //  Here we'll create some zombies which the player can pick-up. They are still part of the same Group.
+        
+        
+        var c = group.create(game.rnd.integerInRange(-1000, 1000), game.rnd.integerInRange(-1000, 1000), 'tank');
+        //c.name='tank'+counter;
+       // c.body.immovable = true;
+        //c.animations.add('move');
+        //c.animations.play('move', 10, true);
+        zombiesLeft++;
+        
+    }*/
+    
+    
+    
+    
+    
     //add group and enable the physics
     group = game.add.group();
     group.enableBody = true;
@@ -106,7 +70,23 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     
+    minions = game.add.group();
+    minions.enableBody = true;
+
+    for (var i = 0; i < 10; i++)
+    {
+        //var minion = minions.create(game.world.randomX, game.world.randomY, 'minion');
+        var minion = group.create(game.rnd.integerInRange(-1000, 1000), game.rnd.integerInRange(-1000, 1000), 'minion', 21);
+        minion.name='innocent'+i;
+        minion.body.velocity.set(200, 200);
+        minion.body.bounce.set(1, 1);
+        minion.body.collideWorldBounds = true;
+        //var c = group.create(game.rnd.integerInRange(-1000, 1000), game.rnd.integerInRange(-1000, 1000), 'innocent', 17);
+    }
     
+    
+    
+    //add the hero and add the anchor to half of the hero's height and weight
     hero = game.add.sprite(game.world.centerX, game.world.centerY, 'hero');
     hero.anchor.set(0.5);
     
@@ -119,9 +99,10 @@ function create() {
     //  And enable the Sprite to have a physics body:
     game.physics.arcade.enable(hero);
     hero.bringToTop();
+    
     //make the camera follow the sprite
     game.camera.follow(hero);
-    game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
+    game.camera.deadzone = new Phaser.Rectangle(400, 200, 500, 300);
     game.camera.focusOnXY(0, 0);
     
     
@@ -133,7 +114,7 @@ function create() {
     {
         //  Here we'll create some chillis which the player can pick-up. They are still part of the same Group.
         
-        //c.name = 'innocent' + i;
+        
         //var c = game.add.sprite(game.rnd.integerInRange(0, 2000), game.rnd.integerInRange(0, 2000), 'innocent', 17);
         var c = group.create(game.rnd.integerInRange(-1000, 1000), game.rnd.integerInRange(-1000, 1000), 'innocent', 17);
         c.name='innocent'+i;
@@ -144,10 +125,13 @@ function create() {
         
     }
     
-    
-   // innocent = game.add.sprite(400, 400, 'innocent');
-    //innocent.animations.add('wave');
-    //innocent.animations.play('wave', 10, true);
+    //  The boss
+   boss = group.create(game.rnd.integerInRange(-1000, 1000), game.rnd.integerInRange(-1000, 1000), 'boss', 20);
+   boss.anchor.setTo(0.5, 0.5);
+   boss.enablebody = true;
+   boss.animations.add('move');
+   boss.animations.play('move', 5, true);
+
 
 
     
@@ -160,15 +144,18 @@ function create() {
 }
 
 function update () {
-
+   
   
     
     game.physics.arcade.collide(hero, group, collisionHandler, null, this);
+    
     game.physics.arcade.collide(group, group);
-
+    
+    //set the hero's speed to 0 initially
     hero.body.velocity.x = 0;
     hero.body.velocity.y = 0;
-
+    
+//change direction and speed of the hero
     
      if (cursors.left.isDown)
     {
@@ -182,7 +169,7 @@ function update () {
     if (cursors.up.isDown)
     {
         //  The speed we'll travel at
-        currentSpeed = 500;
+        currentSpeed = 400;
     }
     else
     {
@@ -200,22 +187,41 @@ function update () {
     land.tilePosition.x = -game.camera.x;
     land.tilePosition.y = -game.camera.y;
     
-      
     
-        
+    /*if (game.input.mousePointer.isDown)
+    {
+        //  First is the callback
+        //  Second is the context in which the callback runs, in this case game.physics.arcade
+        //  Third is the parameter the callback expects - it is always sent the Group child as the first parameter
+        minions.forEach(game.physics.arcade.moveToPointer, game.physics.arcade, false, 200);
+    }
+    else
+    {
+        minions.setAll('body.velocity.x', 0);
+        minions.setAll('body.velocity.y', 0);
+    }*/
+   // minions = group.create(game.rnd.integerInRange(-1000, 1000), game.rnd.integerInRange(-1000, 1000), 'minion', 30);
+    minions.forEach(game.physics.arcade.moveToPointer, game.physics.arcade, false, 100);
+
+    
+    
     
 }
 
 function collisionHandler (player, veg) {
 
     //  If the player collides with the chillis then they get eaten :)
-    //  The chilli frame ID is 17
+    //  The Innocents frame ID is 17
 
     if (veg.frame == 17)
     {
         veg.kill();
         updateText();
         
+    }
+    else if(veg.frame = 20)
+    {
+        player.kill();
     }
 }
 
